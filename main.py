709d077afc7e3abcfc75aef553c020107951b2f3
yuvalsmilo <36788569+yuvalsmilo@users.noreply.github.com>
2024-06-23 19:36:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from landlab.components import OverlandFlow
 from landlab.components import SoilInfiltrationGreenAmpt
 from landlab.io import read_esri_ascii
-
+import matplotlib.pyplot as plt
 # First, read the topo data
 grid_path = './Inputs/LuckyHills103_1m.asc'
 outlet_node = int(14504)
@@ -38,6 +38,16 @@ SI = SoilInfiltrationGreenAmpt(grid, hydraulic_conductivity=Ks,
 
 ## Overland flow component init
 roughness = 0.07
+grid.add_zeros('water_surface__slope',
+               at="node")
+grid.add_zeros('surface_water__depth_at_link',
+               at="link")
+grid.add_zeros('water_surface__elevation',
+               at="node")
+
+grid.at_link['surface_water__depth_at_link'][:] = 10 ** -8
+grid.at_node['surface_water__depth'][:] = 10 ** -8
+
 ld = OverlandFlow(grid, mannings_n = roughness,
                   steep_slopes=True)
 
@@ -50,11 +60,12 @@ int_index = 0   # First rainfall intensity interval
 current_rainfall_duration = rainfall_duration[int_index]
 ld.rainfall_intensity = rainfall_rate[int_index]
 epsilon = 10**-10
-threshold_of_pits = 5 # m^3
+threshold_of_pits = 1 # m^3
 
 # Main loop
 elapse_dts = 0  # counter of simulation time [sec]
 min_dt = 30     # maximal dt [sec] to ensure stability
+wh_at_node = []
 while elapse_dts < rainfall_duration[-1]:
     if elapse_dts >= current_rainfall_duration:  # sec
 
@@ -62,7 +73,7 @@ while elapse_dts < rainfall_duration[-1]:
         current_rainfall_rate = rainfall_rate[int_index]
         current_rainfall_duration = rainfall_duration[int_index]
         ld.rainfall_intensity = current_rainfall_rate  # meter per sec -> For the OverlandFlow component
-
+    wh_at_node.append(grid.at_node['surface_water__depth'][outlet_node+1])
     ld.calc_time_step()
     dt = np.min((ld._dt,min_dt))
     ld.run_one_step(dt=dt)
@@ -80,5 +91,7 @@ while np.sum(grid.at_node['surface_water__depth']) >= threshold_of_pits: # conti
     SI.run_one_step(dt=dt)
     elapse_dts += dt
     print(np.sum(grid.at_node['surface_water__depth']))
+    wh_at_node.append(grid.at_node['surface_water__depth'][outlet_node+1])
 
 
+plt.plot(wh_at_node),plt.show()
